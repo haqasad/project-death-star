7b487a672f0579b5b7f74148c4b1151133011939
haqasad <haq.asad000@gmail.com>
2019-04-12 07:58:13 -0400

    Added BattleSimulator to BattleEngine

---
+++ DevSkill.ProjectDeathStar.BattleEngine/BattleSimulator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevSkill.ProjectDeathStar.BattleEngine
+{
+    public class BattleSimulator
+    {
+        public static BattleResult CalculateBattleResult()
+        {
+
+        }
+    }
+}
