d02fa5716298d998a8725af12d4748be509b4e69
haqasad <haq.asad000@gmail.com>
2019-04-12 05:58:09 -0400

    Added Ship to BattleEngine; added method and properties with half-written code.

---
+++ DevSkill.ProjectDeathStar.BattleEngine/Ship.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevSkill.ProjectDeathStar.BattleEngine
+{
+    public abstract class Ship
+    {
+        public string Name { get; protected set; }
+        public string PictureUrl { get; protected set; }
+        public uint Sheild { get; protected set; }
+        public uint Armor { get; protected set; }
+        public uint Hull { get; protected set; }
+        public uint Range { get; protected set; }
+        public IWeapon[] WeaponSlots { get; protected set; }
+        public EngineeringModule[] EngineeringSlots { get; protected set; }
+        public DefenseModule[] DefenseModuleSlots { get; protected set; }
+
+        public uint CalculateTotalDamage()
+        {
+            uint totalDamage = 0;
+            return 0;
+        }
+    }
+}
