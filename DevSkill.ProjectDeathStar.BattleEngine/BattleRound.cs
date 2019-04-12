bcf2459c4c8ea3d098d7831f7bdae86cb1bed2f8
haqasad <haq.asad000@gmail.com>
2019-04-12 07:25:27 -0400

    Added BattleRound to BattleEngine

---
+++ DevSkill.ProjectDeathStar.BattleEngine/BattleRound.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevSkill.ProjectDeathStar.BattleEngine
+{
+    public class BattleRound
+    {
+        public uint RoundNumber { get; private set; }
+        public BattleRound(uint roundNumber, BattleFormation offensiveFormation, BattleFormation defensiveFormation)
+        {
+
+        }
+    }
+}
