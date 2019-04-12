421a41b14166af14439f92515ae27b9a0ffbe2d5
haqasad <haq.asad000@gmail.com>
2019-04-12 08:13:16 -0400

    Added Planet to BattleEngine

--- DevSkill.ProjectDeathStar.BattleEngine/Planet.cs
+++ DevSkill.ProjectDeathStar.BattleEngine/Planet.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace DevSkill.ProjectDeathStar.BattleEngine
 {
-    class Planet
+    public class Planet
     {
+        public string Name { get; set; }
+        public BattleFormation OffensiveFormation { get; set; }
+        public BattleFormation DefensiveFormation { get; set; }
+
+        private Planet(string name)
+        {
+            Name = name;
+        }
+
+        public static Planet CreatePlanet(string name)
+        {
+            switch (name)
+            {
+                case "Pandora":
+                    return new Planet(name);
+                case "Lava":
+                    return new Planet(name);
+                default:
+                    return null;
+            }
+        }
     }
 }
