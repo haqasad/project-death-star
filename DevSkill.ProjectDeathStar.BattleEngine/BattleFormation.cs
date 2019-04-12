6a024c822f07d728c2f38d053582f35201a65bea
haqasad <haq.asad000@gmail.com>
2019-04-12 05:46:45 -0400

    Added FormationRow to BattleEngine, simple modification added to BattleFormation

--- DevSkill.ProjectDeathStar.BattleEngine/BattleFormation.cs
+++ DevSkill.ProjectDeathStar.BattleEngine/BattleFormation.cs
@@ -4,7 +4,11 @@ using System.Text;
 
 namespace DevSkill.ProjectDeathStar.BattleEngine
 {
-    class BattleFormation
+    public class BattleFormation
     {
+        public string Name { get; set; }
+        public FormationRow[] Rows { get; private set; }
+        public bool IsActive { get; private set; };
+        public General GeneralInCharge { get; private set; };
     }
 }
