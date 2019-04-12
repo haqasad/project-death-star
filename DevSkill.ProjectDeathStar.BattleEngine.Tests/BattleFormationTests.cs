35ac1710a9070705494bc4183c483b1e32bd994d
haqasad <haq.asad000@gmail.com>
2019-04-12 11:17:24 -0400

    Added code to BattleFormation and BattleFormationTests

--- DevSkill.ProjectDeathStar.BattleEngine.Tests/BattleFormationTests.cs
+++ DevSkill.ProjectDeathStar.BattleEngine.Tests/BattleFormationTests.cs
@@ -1,12 +1,25 @@
+using Autofac.Extras.Moq;
+using DevSkill.ProjectDeathStar.BattleEngine;
 using NUnit.Framework;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Tests
 {
+    [TestFixture, ExcludeFromCodeCoverage]
     public class BattleFormationTests
     {
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void SetFormationRow_ValidInput_SetsShip()
+        {
+            using (var mock = AutoMock.GetLoose())
+            {
+                BattleFormation formation = new BattleFormation("formation 1");
+            }
         }
 
         [Test]
--- DevSkill.ProjectDeathStar.BattleEngine/BattleFormation.cs
+++ DevSkill.ProjectDeathStar.BattleEngine/BattleFormation.cs
@@ -10,5 +10,28 @@ namespace DevSkill.ProjectDeathStar.BattleEngine
         public FormationRow[] Rows { get; private set; }
         public bool IsActive { get; private set; }
         public General GeneralInCharge { get; private set; }
+
+        public BattleFormation(string name)
+        {
+            Name = name;
+            Rows = new FormationRow[6];
+        }
+
+        public void SetFormationRow(Ship seletedShip, uint amountOfShips, uint rowPosition)
+        {
+            if (rowPosition < 6)
+            {
+                Rows[rowPosition] = new FormationRow(seletedShip, amountOfShips);
+            }
+            else
+                throw new Exception("Invalid Row Position");
+        }
+
+        public void ActivateFormation(General assignedGeneral)
+        {
+            GeneralInCharge = assignedGeneral;
+            IsActive = true;
+        }
+
     }
 }
