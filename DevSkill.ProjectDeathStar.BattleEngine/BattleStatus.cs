7bc6dee2bef019e5daf7e6dfa06157d3df325ca8
haqasad <haq.asad000@gmail.com>
2019-04-12 07:15:09 -0400

    Added BattleStatus to BattleEngine

---
+++ DevSkill.ProjectDeathStar.BattleEngine/BattleStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevSkill.ProjectDeathStar.BattleEngine
+{
+    public enum BattleStatus
+    {
+        OffenseSuccessful,
+        DefenseSuccessful,
+        Draw
+    }
+}
